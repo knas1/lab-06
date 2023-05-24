aba4eedd10d059b34dbd790741e508170a4c5f5a
Nawaf Alghamdi <nawafgh33@gmail.com>
2023-05-24 17:28:26 +0300

    BONUS BADGES

---
+++ README.md
@@ -0,0 +1,8 @@
+[![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-24ddc0f5d75046c5622901739e7c5dd533143b0c8e959d652212380cedb1ea36.svg)](https://classroom.github.com/a/ouBX2pFd)
+
+
+Q2. The Decorator design pattern is better than creating subclasses for each combination of Transport and Formatter because it provides flexibility, scalability, promotes code reusability, and follows the Single Responsibility Principle.
+
+
+
+
--- src/main/java/sa/edu/kau/fcit/cpit252/logger/formatter/YAMLFormatter.java
+++ src/main/java/sa/edu/kau/fcit/cpit252/logger/formatter/YAMLFormatter.java
@@ -1,12 +1,17 @@
 package sa.edu.kau.fcit.cpit252.logger.formatter;
 
-public class YAMLFormatter {
+import sa.edu.kau.fcit.cpit252.logger.handlers.BaseLogger;
 
+public class YAMLFormatter extends FormatDecorator{
+
+    public YAMLFormatter(BaseLogger logger){
+        super.logger = logger;
+    }
     public String getLabel() {
-        return ", YAMLFormatter";
+        return logger.getLabel() + ", YAMLFormatter";
     }
 
     public String getLevel() {
-        return "Info ";
+        return "Info " + logger.getLevel();
     }
 }
